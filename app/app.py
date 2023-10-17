6f4b1eb22d62a569c657cc1969515d414d1dbd95
mario.r99 <mario.srx99@gmail.com>
2023-10-17 03:25:21 -0500

    Add google maps

--- app/app.py
+++ app/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
 # Initialize app
 load_dotenv()
 app = Flask(__name__)
+title = "Chicago Crime Map"
 
 # Set up main route
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def load_map():
+    return render_template(
+        "map.html",
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        center="41.87820816040039,-87.62979125976562",
+        title=title
+    )
