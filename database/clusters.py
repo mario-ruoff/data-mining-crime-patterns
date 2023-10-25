7ebd5f62f90d370a9d65d4418c442e21eccb3ce7
Zak Kastl <zkastl8@gmail.com>
2023-10-25 15:30:10 -0500

    added debug file; implemented kmeans via sklearn

--- database/clusters.py
+++ database/clusters.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import sqlite3
 
+from sklearn.cluster import KMeans
+
 class ChicagoCrimes:
     def __init__(self, db_file) -> None:
         self.db_file = db_file
@@ -22,7 +24,6 @@ class ChicagoCrimes:
         raw_results = cursor.execute(query)
         result = raw_results.fetchone()
         while(result is not None):
-            print(result)
             self.current_results.append((result[0], result[1]))
             result = raw_results.fetchone()
 
@@ -30,23 +31,30 @@ class ChicagoCrimes:
     
     def get_crimes(self, sql_query=None):
 
-            # something something database
-            db_connection = sqlite3.connect(self.db_file)
-            cursor = db_connection.cursor()
+        # something something database
+        db_connection = sqlite3.connect(self.db_file)
+        cursor = db_connection.cursor()
 
-            query = '''
-                SELECT latitude,longitude, date, primary_type from crimes
-                WHERE arrest=0 and primary_type='ASSAULT' and year=2023
-                ORDER BY date
-                LIMIT 1000
-                '''
-            
-            self.current_results = []
-            raw_results = cursor.execute(query)
+        query = '''
+            SELECT latitude,longitude, date, primary_type from crimes
+            WHERE arrest=0 and primary_type='ASSAULT' and year=2023
+            ORDER BY date
+            LIMIT 1000000
+            '''
+        
+        self.current_results = []
+        raw_results = cursor.execute(query)
+        result = raw_results.fetchone()
+
+        while(result is not None):
+            self.current_results.append((result[0], result[1]))
             result = raw_results.fetchone()
-            while(result is not None):
-                print(result)
-                self.current_results.append((result[0], result[1]))
-                result = raw_results.fetchone()
 
-            return self.current_results
+        record_array = np.array(self.current_results)
+        locations = record_array[:, (0,1)]
+
+        k = KMeans(n_clusters=20, n_init='auto')
+        k.fit(locations)
+        clusters = k.cluster_centers_
+
+        return self.current_results, clusters
