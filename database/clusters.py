fc2e324466b33bed28b4b6011908bc6ff695715c
Zak Kastl <zkastl8@gmail.com>
2023-12-03 12:40:17 -0600

    added kmeans algorithm

--- database/clusters.py
+++ database/clusters.py
@@ -2,6 +2,7 @@ import numpy as np
 import sqlite3
 
 from sklearn.cluster import KMeans
+#from local_k_means import KMeans4
 
 class ChicagoCrimes:
     def __init__(self, db_file) -> None:
@@ -63,10 +64,12 @@ class ChicagoCrimes:
         if len(locations) < k:
             return self.current_results, None
 
-        # Apply clustering
+        #Apply clustering
         k = KMeans(n_clusters=k, n_init='auto')
         k.fit(locations)
         clusters = k.cluster_centers_
+        kmeans = KMeans4(n_clusters=k)
+        #clusters, centroids = kmeans.fit2(locations)
 
         return self.current_results, clusters
     
@@ -91,6 +94,49 @@ class ChicagoCrimes:
             first = query_task.fetchone()
 
         return results
+
+'''
+KMeans4: An self-implemented version of kmeans
+
+This method will utilize similar signitures to the scikit-learn
+package: 'KMeans' in order to provide similar results. It will not
+be optimized.
+'''
+class KMeans4:
+    def __init__(self, n_clusters, max_iter=300):
+        self.k = n_clusters
+        self.max_iter = max_iter
+
+    def fit(self, data):
+        # Randomly choose centroids from the data
+        copy = np.copy(data)
+        np.random.shuffle(copy)
+        centroids = copy[:self.k]
+
+        for c in copy:
+            pass
+
+    def fit2(self, X, tolerance=1e-4):
+        # Step 1: Initialize centroids randomly from the dataset
+        centroids = X[np.random.choice(X.shape[0], self.k, replace=False)]
+
+        for _ in range(self.max_iter):
+            # Step 2: Assign points to the nearest centroid
+            distances = np.sqrt(((X - centroids[:, np.newaxis])**2).sum(axis=2))
+            closest_centroids = np.argmin(distances, axis=0)
+
+            # Step 3: Update centroids to be the mean of assigned points
+            new_centroids = np.array([X[closest_centroids == i].mean(axis=0) for i in range(self.k)])
+
+            # Check for convergence (if centroids don't change)
+            if np.all(np.abs(new_centroids - centroids) < tolerance):
+                for c in centroids:
+                    print(f'Centroid: {c}')
+                break
+
+            centroids = new_centroids
+
+        return closest_centroids, centroids
     
 if __name__ == '__main__':
     crimes_test = ChicagoCrimes('./crimes.db')
